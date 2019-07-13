9b8c5bc67e44f58f0de588efd4a56bd4a40c83ae
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-13 19:29:18 -0400

    Solved climbStairs challenge

---
+++ algos_data-structures/Recursion/Dynamic Programming/climbStairs.js
@@ -0,0 +1,56 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+*/
+
+// 3 Steps to use dynamic programming
+
+
+// First find recursive solution
+
+const climbStairs = function(n) {
+
+    if (n < 2){
+        return 1;
+    }
+    
+    return climbStairs(n - 2) + climbStairs(n - 1)
+};
+
+// Step #2 is to find memoize solution
+
+const climbStairs = function (n) {
+    if (n < 2){
+        return 1;
+    }
+
+    fib = [];
+    fib[0] = 1;
+    fib[1] = 1;
+
+    for (let i = 2; i < n + 1; i++){
+        fib[i] = fib[i-2] + fib[i-1];
+    }
+    return fib[n];
+}
+
+// Step #3 solve with dynamic progrmaming
