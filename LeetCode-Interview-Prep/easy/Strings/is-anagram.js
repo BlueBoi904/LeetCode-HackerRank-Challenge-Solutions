fd3da3b6e364b5a5e5df06436fd87eb5935e894b
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-15 17:53:45 -0400

    finished anagram challenge

---
+++ LeetCode-Interview-Prep/easy/Strings/is-anagram.js
@@ -0,0 +1,40 @@
+/*
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+Note:
+You may assume the string contains only lowercase alphabets.
+
+Follow up:
+What if the inputs contain unicode characters? How would you adapt your solution to such case?
+*/
+// Solve using built-in javascript functionality
+
+var isAnagram = function(s, t) {
+    s = s.replace(/[\W]/g, '').split('').sort().join('');
+    t = t.replace(/[\W]/g, '').split('').sort().join('');
+    return s === t;
+};
+
+// Solve using data structure
+
+const isAnagram = function(s,t){
+    if (s.length !== t.length){
+        return false;
+    } 
+
+    let counts = [];
+
+    for (let i = 0; i < s.length; i++){
+        counts[s.charAt(i) - 'a']++;
+    }
+
+}
+
